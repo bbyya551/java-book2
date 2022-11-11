b9649564b39ac02e5c66bd1557f4b224021c4cc1
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-11-12 00:11:07 +0900

    [Add]403

---
+++ Ex403.java
@@ -0,0 +1,13 @@
+
+public class Ex403 {
+	public static void main(String[] args) {
+		int[] numbers = new int[] { 3, 4, 9 };
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] == input) {
+				System.out.println("アタリ！");
+			}
+		}
+	}
+}
