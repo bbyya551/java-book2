92cb254fb44567e6d4dceffbf8dbe477c2084eef
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-11-12 23:22:33 +0900

    [Edit]

---
+++ Book501.java
@@ -0,0 +1,6 @@
+
+public class Book501 {
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
+	}
+}
---
+++ Book503.java
@@ -0,0 +1,13 @@
+
+public class Book503 {
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	public static void methodB() {
+		System.out.println("methodB");
+	}
+	public static void main(String[] args) {
+		methodA();
+	}
+}
