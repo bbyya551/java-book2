5647b878b6a22eccf303f6eed739a4935aad2004
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-11-13 22:48:12 +0900

    [Add]502

---
+++ Ex501.java
@@ -0,0 +1,13 @@
+
+public class Ex501 {
+	public static void introduceOneself() {
+		String name = "abe";
+		int age = 26;
+		double height = 166.9;
+		char zodiac = '牛';
+		System.out.println("名前は" + name + "です。年齢は" + age + "歳です。身長は" + height + "で、十二支は" + zodiac + "です。");
+	}
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+}
---
+++ Ex502.java
@@ -0,0 +1,15 @@
+
+public class Ex502 {
+	public static void email(String title, String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名:" + title);
+		System.out.println("本文:" + text);
+	}
+	
+	public static void main(String[] args) {
+		String title = "こんにちは";
+		String address = "a@a";
+		String text = "ようこそ";
+		email(title, address, text);
+	}
+}
