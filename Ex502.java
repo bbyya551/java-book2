b3be0978f21abe0d13050593f0f63a4ac1fdc9c1
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-11-14 00:11:45 +0900

    [Edit]502

--- Ex502.java
+++ Ex502.java
@@ -6,10 +6,17 @@ public class Ex502 {
 		System.out.println("本文:" + text);
 	}
 	
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名: 無題");
+		System.out.println("本文:" + text);
+	}
+	
 	public static void main(String[] args) {
 		String title = "こんにちは";
 		String address = "a@a";
 		String text = "ようこそ";
-		email(title, address, text);
+//		email(title, address, text);
+		email(address, text);
 	}
 }
